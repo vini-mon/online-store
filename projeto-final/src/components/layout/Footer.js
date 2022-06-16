59a9305b3ef79372e5dd8b279da87fe461e61d29
João Pedro Rodrigues <joaoprfreitas23@gmail.com>
2022-06-16 18:38:01 -0300

    Footer feito

---
+++ projeto-final/src/components/layout/Footer.js
@@ -0,0 +1,26 @@
+import { FaFacebookSquare, FaInstagram, FaWhatsapp } from 'react-icons/fa'
+
+import styles from './Footer.module.css'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <ul className={styles.social_list}>
+                <li>
+                    <FaFacebookSquare/>
+                </li>
+                <li>
+                    <FaInstagram/>
+                </li>
+                <li>
+                    <FaWhatsapp/>
+                </li>
+            </ul>
+            <p className={styles.copy_right}>
+                &copy;<span>Copyright. All right reserved.</span>
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
