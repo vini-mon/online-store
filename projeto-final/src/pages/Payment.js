7bb4efa1db158e3a6c3f7aba069b62c25dd8898d
akyukyu <gabriel.urakawa@usp.br>
2022-06-18 23:33:00 -0300

    Carrinho

--- projeto-final/src/pages/Payment.js
+++ projeto-final/src/pages/Payment.js
@@ -10,23 +10,45 @@ function Payment(){
         total += product.qnt * product.price;
     });
 
+    const pay = () => {
+        console.log("pay");
+    }
+    const attSupplies = () => {
+        console.log("att")
+    }
+
     const navigate = useNavigate();
     const cancel = () => {
         navigate('/confirm');
     }
     const ahead = () => {
+        attSupplies();
         navigate('/');
     }
 
+
     return(
         <div>
-            <h1 className={styles.title}>Selecione o Método de Pagamento:</h1>
+            <h1 className={styles.title}>Finalize seu Pedido</h1>
             <div className={styles.box}>
-                
+                <h3>Selecione o Detalhes de Pagamento:</h3>
+                <form onSubmit={pay}>
+                    <div className={styles.form}>
+                        <label>Informações do Cliente:</label>
+                        <input type="text" placeholder="Número do Cartão"/>
+                        <input type="text" placeholder="Email"/>
+                        <input type="text" placeholder="CPF"/>
+                        <label>Informações do Cartão:</label>
+                        <input type="text" placeholder="Nome do Titular"/>
+                        <input type="text" placeholder="Código de Segurança"/>
+                        <input type="text" placeholder="Data de Validade"/>
+                    </div>
+                        
+                </form>
             </div>
             <div className={styles.navegation}>
                 <Button text="Voltar" event={cancel}></Button>
-                <Button text="Avançar" event={ahead}></Button>
+                <Button text="Finalizar" event={ahead}></Button>
             </div>
         </div>
 
