d420100f508c893a30e19a0156ffc51baa8329e6
akyukyu <gabriel.urakawa@usp.br>
2022-06-18 22:10:55 -0300

    rota pagamento

--- projeto-final/src/pages/Confirm.js
+++ projeto-final/src/pages/Confirm.js
@@ -1,10 +1,10 @@
-import styles from "./Payment.module.css"
+import styles from "./Confirm.module.css"
 import cartList from '../json/cart';
 import Button from '../components/Button'
 
 import { useNavigate } from 'react-router-dom';
 
-function Payment(){
+function Confirm(){
     let total = 0;
     cartList.map(product => {
         total += product.qnt * product.price;
@@ -12,17 +12,24 @@ function Payment(){
 
     const navigate = useNavigate();
     const cancel = () => {
-        navigate('/confirm');
+        navigate('/cart');
     }
     const ahead = () => {
-        navigate('/');
+        navigate('/payment');
     }
 
     return(
         <div>
-            <h1 className={styles.title}>Selecione o Método de Pagamento:</h1>
+            <h1 className={styles.title}>Finalize seu Pedido</h1>
             <div className={styles.box}>
-                
+                <h3>Confira seu Pedido:</h3>
+                {cartList.map((cart) => (
+                    <div className={styles.paymentBox} key={cart.id}>
+                        <p>{cart.qnt}x {cart.name} R${cart.price * cart.qnt}</p>
+                        <button className={styles.btn}>X</button>
+                    </div>                         
+                ))}
+                <p>Total: R${total}</p>
             </div>
             <div className={styles.navegation}>
                 <Button text="Voltar" event={cancel}></Button>
@@ -34,4 +41,4 @@ function Payment(){
     );
 };
 
-export default Payment
+export default Confirm
