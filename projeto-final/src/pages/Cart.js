5912286dcdcb0f8c4525ee9d4fc6bad4eceedf19
João Pedro Rodrigues <joaoprfreitas23@gmail.com>
2022-06-19 11:47:03 -0300

    Alterações no cart

--- projeto-final/src/pages/Cart.js
+++ projeto-final/src/pages/Cart.js
@@ -14,30 +14,25 @@ function Cart() {
     });
 
     const navigate = useNavigate();
+    
     const payment = () => {
         navigate('/payment');
     }
 
-    const handleAdd = (product) => {
+    const handleAdd = () => {
         console.log("add");
     }
 
-    const handleRemove = (product) => {
+    const handleRemove = () => {
         console.log("remove");
     }
 
-    return(
-
+    return (
         <div>
-
-            <p className={styles.breadcrumb}><span className={styles.green} >Carrinho</span> <i class="fa-solid fa-circle-right"></i> Pagamento </p>
-
+            <p className={styles.breadcrumb}><span className={styles.green}>Carrinho</span> <i class="fa-solid fa-circle-right"></i> Pagamento </p>
             <h1 className={styles.title}>Carrinho</h1>
-
             <div className={styles.box}>
-
                 {cartList.map((cart) => (
-
                     <CartProduct
                         name={cart.name} 
                         description={cart.description}
@@ -48,14 +43,10 @@ function Cart() {
                         eventAdd={handleAdd}
                         eventRemove={handleRemove}
                     />      
-
                 ))}
-
                 <div className={styles.end}>
-                    
                     <span className={styles.total}> Total: R${total}.00 </span>
                     <button text="Finalizar Compra" className={styles.btn} onClick={payment}>Finalizar compra</button>
-
                 </div>
             </div>
         </div>
