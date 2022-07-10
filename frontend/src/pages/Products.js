53cb9d55e2a57fbb1e4daa8543554114d9377483
Vinícius Santos Monteiro <vini.mon@usp.br>
2022-07-10 01:14:26 -0300

    toast e sound dos produtos funcionando corretamente

--- frontend/src/pages/Products.js
+++ frontend/src/pages/Products.js
@@ -6,10 +6,14 @@ import capa from '../img/products/Products.png';
 import useAxios from "../hooks/useAxios";
 import axios from "../api/axiosInstance";
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 // função que comanda as ações da paginas de produtos
 // realiza a adição de produtos ao carrinho (armazenamento no localstorage)
 // return: HTML da pagina de Products
 function Products() {
+
     const [products, error, loading] = useAxios({
         axiosInstance: axios,
         method: 'GET',
@@ -19,16 +23,42 @@ function Products() {
         }
     })
 
+    const notify = () => toast('Produto adicionado ao carrinho!', {
+        position: "bottom-left",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+    });
+
+    const stylesToast = {
+
+        backgroundColor: '#fbc2eb',
+        boxShadow: '0 0 50px 1px #f296d8',
+        border: '1px solid #f296d8',
+        color: '#9d52f9',
+
+    }
+
     let cartList = JSON.parse(localStorage.getItem('ProductList'));
+
     const handleClick = (id) => {
+
+        notify();
+
         if (cartList === null) cartList = {};
         
         cartList[id] = cartList[id] ? cartList[id] + 1 : 1;
         localStorage.setItem('ProductList', JSON.stringify(cartList))
+
     }
     
     return (
         <div>
+            <div><ToastContainer toastStyle={stylesToast} pauseOnFocusLoss={false} /></div>
             <div className={styles.intro}>
                 <div className={styles.text}>
                     <h1>Conheça nossos produtos<br/>para seu PET</h1>
